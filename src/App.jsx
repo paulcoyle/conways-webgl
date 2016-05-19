e747a1d8340f76bbf3d5d86c1a8d966b606ea725
Paul Coyle <paul@paulcoyle.com>
2016-05-18 21:40:45 -0600

    Stub remaining UI and refine styling.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ var React = require('react')
   , ReactDOM = require('react-dom')
   , Render = require('./Render')
   , Controls = require('./Controls')
+  , Rules = require('../lib/Rules')
   , App
   ;
 
@@ -10,9 +11,24 @@ require('./App.styl');
 App = React.createClass({
   getInitialState() {
     return {
+      ruleSets: Rules.ruleSets,
+      currentRuleSetIndex: 0,
       playing: false,
-      speed: 1
+      speed: 1,
+      scale: 1,
+      offset: {
+        x: 0, y: 0
+      }
     };
+  },
+
+  currentRuleSet() {
+    return this.state.ruleSets[this.state.currentRuleSetIndex];
+  },
+
+  handleRuleSetChange(ruleSetIndex) {
+    this.state.currentRuleSetIndex = ruleSetIndex;
+    this.setState(this.state);
   },
 
   handlePlay() {
@@ -30,16 +46,38 @@ App = React.createClass({
     this.setState(this.state);
   },
 
+  handleDisplayReset() {
+    this.state.scale = 1;
+    this.setState(this.state);
+  },
+
+  handleScaleChange(newScale) {
+    this.state.scale = newScale;
+    this.setState(this.state);
+  },
+
   render() {
     return (
       <div id="container">
-        <Render appState={this.state} />
-        <Controls
+        <Render
+          ruleSet={this.currentRuleSet()}
           playing={this.state.playing}
           speed={this.state.speed}
+          scale={this.state.scale}
+          offset={this.state.offset} />
+
+        <Controls
+          ruleSets={this.state.ruleSets}
+          currentRuleSetIndex={this.state.currentRuleSetIndex}
+          playing={this.state.playing}
+          speed={this.state.speed}
+          scale={this.state.scale}
+          onRuleSetChange={this.handleRuleSetChange}
           onPlay={this.handlePlay}
           onStop={this.handleStop}
-          onSpeedChange={this.handleSpeedChange} />
+          onSpeedChange={this.handleSpeedChange}
+          onDisplayReset={this.handleDisplayReset}
+          onScaleChange={this.handleScaleChange} />
       </div>
     );
   }
--- src/Controls.jsx
+++ src/Controls.jsx
@@ -8,17 +8,24 @@ require('./Controls.styl');
 module.exports = React.createClass({
   getInitialState() {
     return {
-      speedRange: {min: 0, max: 2}
+      speedRange: {min: 0, max: 2},
+      scaleRange: {min: 1, max: 10}
     };
   },
 
   getDefaultProps() {
     return {
+      ruleSets: [],
+      currentRuleSetIndex: -1,
       playing: false,
       speed: 0,
+      scale: 1,
+      onRuleSetChange: () => {},
       onPlay: () => {},
       onStop: () => {},
-      onSpeedChange: () => {}
+      onSpeedChange: () => {},
+      onDisplayReset: () => {},
+      onScaleChange: () => {}
     };
   },
 
@@ -28,6 +35,16 @@ module.exports = React.createClass({
     } else {
       return "Play";
     }
+  },
+
+  ruleOptions() {
+    return this.props.ruleSets.map((rule, i) => {
+      return <option key={i} value={i}>{rule.label}</option>
+    });
+  },
+
+  handleRuleChange(e) {
+    this.props.onRuleSetChange(e.target.value);
   },
 
   handlePlayStop() {
@@ -42,11 +59,30 @@ module.exports = React.createClass({
     this.props.onSpeedChange(newSpeed);
   },
 
+  handleDisplayReset() {
+    this.props.onDisplayReset();
+  },
+
+  handleScaleChange(newScale) {
+    this.props.onScaleChange(newScale);
+  },
+
   render() {
     return (
       <div className="controls-container">
         <div className="control-group">
-          <p>Playback</p>
+          <p>Initial State</p>
+          <button type="button">Clear</button>
+          <button type="button">Seed</button>
+        </div>
+        <div className="control-group">
+          <p>Rules</p>
+          <select value={this.props.currentRuleSetIndex} onChange={this.handleRuleChange}>
+            {this.ruleOptions()}
+          </select>
+        </div>
+        <div className="control-group">
+          <p>Simulation</p>
           <button type="button" disabled={this.props.playing}>Step</button>
           <button type="button" onClick={this.handlePlayStop}>{this.getPlayStopLabel()}</button>
           <RangeControl
@@ -57,6 +93,17 @@ module.exports = React.createClass({
             value={this.props.speed}
             valueLabel={SPEED_LABELS[this.props.speed]}
             onChange={this.handleSpeedChange} />
+        </div>
+        <div className="control-group">
+          <p>Display</p>
+          <button type="button" onClick={this.handleDisplayReset}>Reset</button>
+          <RangeControl
+            id="scale"
+            label="Scale"
+            range={this.state.scaleRange}
+            step="0.1"
+            value={this.props.scale}
+            onChange={this.handleScaleChange} />
         </div>
       </div>
     );
